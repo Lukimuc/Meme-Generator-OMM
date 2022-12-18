7cd02e0df272a5f6d538d4603dbdd1bd6cbf32b6
Tobias Nitzl <tobi.nitzl@googlemail.com>
2022-12-18 10:32:13 +0100

    Routing react

---
+++ react-frontend/src/pages/Singleview/singleview.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Singleview extends React.Component {
+
+    render () {
+        return (
+            <div> Singleview ...</div>
+        )
+    }
+}
+
+export default Singleview
