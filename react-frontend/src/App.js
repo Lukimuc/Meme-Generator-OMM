bd3d1d538591c86689538e9c72fa624bb1477fc5
Lukas Schneider <lukas.schneider96@gmail.com>
2023-01-07 11:06:43 +0100

    material-ui button test works

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Signin from './Pages/Signin/Signin';
 import Register from './Pages/Register/Register';
 import Home from './Pages/Home/Home';
+import Button from '@mui/material/Button';
 
 class App extends Component {
   constructor() {
@@ -28,7 +29,7 @@ class App extends Component {
     return (
       <>
         {/* todo Navigation einfügen */}
-
+        <Button variant="contained">Hello World</Button>
         { this.state.route === 'home'
         ? <Home onRouteChange={this.onRouteChange}/>
         : (this.state.route === 'signin'
