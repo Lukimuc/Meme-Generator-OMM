b0ce134997bcc7e6296634267ab0b77898391d0b
Lukas Schneider <lukas.schneider96@gmail.com>
2023-01-08 18:42:21 +0100

    deleted fake database added mongodb database
    user registration works (without names, just email and pw)

--- node-backend/app.js
+++ node-backend/app.js
@@ -9,18 +9,124 @@ var cors = require('cors');
 // ##### IMPORTANT
 // ### Your backend project has to switch the MongoDB port like this
 // ### Thus copy paste this block to your project
-const MONGODB_PORT = process.env.DBPORT || '27017';
+/* const MONGODB_PORT = process.env.DBPORT || '27017';
 const db = require('monk')(`127.0.0.1:${MONGODB_PORT}/omm-2223`); // connect to database omm-2021
 console.log(`Connected to MongoDB at port ${MONGODB_PORT}`)
 // ######
+console.log(db);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const e = require('express');
 
-var app = express();
+var app = express(); */
 
 /* ------------ Lukas Test -------------- */
+
+// mongoDB Test
+// app.js (or index.js)
+//const express = require('express')
+const app = express()
+const port = 3002
+
+app.listen(port, () => console.log('Example app listening on port ' + port))
+
+// Setup  MongoDB:
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = 'mongodb://localhost:27017';
+
+const dbName = 'memeGeneratorDB';
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect()
+
+// async function main() {
+//   /* // Setup  MongoDB:
+//   const MongoClient = require('mongodb').MongoClient;
+//   const assert = require('assert');
+//   const url = 'mongodb://localhost:27017';
+
+//   const dbName = 'memeGeneratorDB';
+//   const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true }); */
+
+//   try {
+//     await client.connect(); // gets back a promise, it waits for it bc of the await keyword
+//     /* await createUser(client,
+//       {
+//         "name": "testUserCreatedbyMethod",
+//         "email": "testUserCreatedbyMethod",
+//         "password": "testUserCreatedbyMethod"
+//       }); */
+
+//     // await findOneUserByEmail(client,"john@test.de");
+
+//     // await updateOneUserByEmail(client,"john@test.de", {entries: 6});
+
+//     //  await deleteOneUserByEmail(client, "testUserCreatedbyMethod")
+//   } catch (error) {
+//     console.error(error);
+//   } finally { // finally: regardles of the outcome it executes and closes the connection
+//     await client.close();
+//   }
+// }
+
+// main().catch(console.error);
+
+async function listDatabases(client) {
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+// create User Document in DB
+async function createUser(client, newUser) {
+  const result = await client.db("memeGeneratorDB").collection("users").insertOne(newUser);
+  console.log(`New User created with the ID ${result.insertedID}`);
+
+  return result;
+}
+
+// get a specific User by email in DB
+async function findOneUserByEmail(client, email) {
+  const result = await client.db("memeGeneratorDB").collection("users").findOne({ 'email': email });
+
+  if (result) {
+    console.log(`found a user in the collection with the email ${email}`);
+    console.log(result);
+  } else {
+    console.log(`No user found with the email ${email}`);
+  } return result;
+}
+
+// update a specific User by email in DB
+async function updateOneUserByEmail(client, email, updatedUser) {
+  const result = await client.db("memeGeneratorDB").collection("users").updateOne({ email: email }, { $set: updatedUser });
+
+  console.log(`${result.matchedCount} documents matched the query criteria`);
+  console.log(`${result.modifiedCount} documents were updated`);
+  return result;
+}
+
+// upsert a specific User by email in DB -> If the user exists update the attribute, otherwise create a new user
+async function upsertOneUserByEmail(client, email, updatedUser) {
+  const result = await client.db("memeGeneratorDB").collection("users").updateOne({ email: email }, { $set: updatedUser }, { upsert: true });
+
+  console.log(`${result.matchedCount} documents matched the query criteria`);
+  console.log(`${result.modifiedCount} documents were updated`);
+  return result;
+}
+
+
+async function deleteOneUserByEmail(client, email) {
+  const result = await client.db("memeGeneratorDB").collection("users").deleteOne({ email: email });
+  console.log(`${result.deletedCount} documents got deleted`);
+  return result;
+}
+
+
+app.use(express.json());
+app.use(cors());
 
 /* Endpoints: 
   /register  ---> Post --> Return: User 
@@ -30,47 +136,24 @@ var app = express();
   / --> GET --> Return ALL User
 */
 
-app.use(express.json());
-app.use(cors());
-const database = { // fake database - just for testing purposes
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'john@test.de',
-      password: 'test',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      email: 'sally@test.de',
-      password: 'test',
-      entries: 0,
-      joined: new Date()
-    }
-  ]
-}
-
 // register new user -> ID still hard coded
-
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-
-  database.users.push({
-    id: '125',
-    name: name,
-    email: email,
-
-    entries: 0,
-    joined: new Date()
-  })
-  res.json(database.users[database.users.length - 1]) // send the user back - change that later
+  newUser = {
+    "name": name,
+    "email": email,
+    "password": password,
+    "entries": 0,
+    "pwHash": "",
+    "userCreated": new Date()
+  }
+  createUser(client, newUser);
+  res.json("Success: New User created")
 })
 
 // login, check (only!) the first user within fake database on the top 
 app.post('/signin', (req, res) => {
+  
   if (req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password) {
     res.json(database.users[0]);
@@ -80,10 +163,12 @@ app.post('/signin', (req, res) => {
 })
 
 // get all users out of database - REMOVE LATER
-app.get('/', (req, res) => {
-  res.send(database.users);
+app.get('/', async (req, res) => {
+  //res.send(database.users);
+  const result = await findOneUserByEmail(client, "john@test.de")
+  console.log(result);
+  res.json(result);
 })
-
 
 // get the individual profil with defined IDs
 app.get('/profile/:id', (req, res) => {
@@ -119,6 +204,8 @@ app.put('/image', (req, res) => {
 /* ------------ Lukas Test Ende -------------- */
 
 
+// ????????? is the following part required ??????????
+/*
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -173,7 +260,7 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
+});*/
 
 module.exports = app;
 
