1046789785ddb30e5223eb294b5e742f5c002ef9
Tobias Nitzl <t.nitzl@campus.lmu.de>
2023-01-08 23:01:22 +0100

    Editor erweitert mit zusätzlichen tests in new_Editor

--- react-frontend/src/pages/Editor/editor.js
+++ react-frontend/src/pages/Editor/editor.js
@@ -1,63 +1,34 @@
 import React, { useState } from 'react'
-import { Stage, Layer, Text, Image } from 'react-konva';
+import { Stage, Layer, Text } from 'react-konva';
+import My_Image from './my_image';
+import My_Text from './my_text';
 
 
-class URLImage extends React.Component {
-    state = {
-      image: null,
-    };
-    componentDidMount() {
-      this.loadImage();
-    }
-    componentDidUpdate(oldProps) {
-      if (oldProps.src !== this.props.src) {
-        this.loadImage();
-      }
-    }
-    componentWillUnmount() {
-      this.image.removeEventListener('load', this.handleLoad);
-    }
-    loadImage() {
-      // save to "this" to remove "load" handler on unmount
-      this.image = new window.Image();
-      this.image.src = this.props.src;
-      this.image.addEventListener('load', this.handleLoad);
-    }
-    handleLoad = () => {
-      // after setState react-konva will update canvas and redraw the layer
-      // because "image" property is changed
-      this.setState({
-        image: this.image,
-      });
-      // if you keep same image object during source updates
-      // you will have to update layer manually:
-      // this.imageNode.getLayer().batchDraw();
-    };
-    render() {
-      return (
-        <Image
-          x={this.props.x}
-          y={this.props.y}
-          image={this.state.image}
-          ref={(node) => {
-            this.imageNode = node;
-          }}
-          draggable={true}
-        />
-      );
-    }
-  }
+
 const Editor = () => {
-    //const dragUrl = React.useRef();
-    const stageRef = React.useRef();
-    
-    //const width = window.innerWidth
-    const height = window.innerHeight
 
-    const [inputtext, setInputtext] = useState()
-    const [inputs, setInputs] = useState([])
-    const [imagesrc, setImageSrc] = useState()
+    // Variablen für Texte
+    const [input_text, setInputs_Text] = useState()
+    const [mytexts, setMytexts] = useState([])
+
+    // Variables changing text
+    const [width, setWidth] = useState(400);
+    const [height, setHeight] = useState(400);
+    // Variablen für images
+    const [input_image, setInput_Image] = useState()
     const [images, setImages] = useState([])
+
+
+
+    // Selecting shapes
+    const [selectedId, selectShape] = useState(null);
+    const checkDeselect = (e) => {
+        // deselect when clicked on empty area
+        const clickedOnEmpty = e.target === e.target.getStage();
+        if (clickedOnEmpty) {
+          selectShape(null);
+        }
+    };
 
     return (
         <div>
@@ -68,30 +39,70 @@ const Editor = () => {
                 display: "block",
                 float: "left",
                 border: "5px outset blue",
-                height: height,
+                height: "100%",
                 width: "50%",
                 }}
             >
                 <Stage 
-                    width={window.innerHeight}
+                    width={window.innerWidth / 2}
                     height={window.innerHeight}
                     style={{
                         border: "1px outset red",
                     }}
-                    ref={stageRef}>
+                    onMouseDown={checkDeselect}
+                    onTouchStart={checkDeselect}
+                 >
                     <Layer>
-                        {inputs.map( (input) => { return <Text key={input} text={input} draggable={true} />}) }
-                        {images.map( (image) => { return <URLImage src={image} /> }) }
+                        {images.map( (img, i) => {
+                            return (
+                                <My_Image 
+                                    key={i}
+                                    imageUrl={img.src}
+                                    isSelected={img.key === selectedId}
+                                    onSelect={() => { selectShape(img.key) }}
+                                    onChange={newAttrs => {
+                                        const imgs = images.slice();
+                                        imgs[i] = newAttrs;
+                                    }}
+                                    width={i === 0 ? window.innerWidth * 0.5 : img.width}
+                                    height={i === 0 ? window.innerHeight: img.height}
+                                />
+                            );
+                        })}
+                        {mytexts.map( (text, i) => {
+                            return(
+                                <My_Text 
+                                    x={50}
+                                    y={50}
+                                    text={text.text}
+                                    colour="#FFDAE1"
+                                    onTextChange={
+                                        // Hier muss der Text angepasst werden , dass er sich ändert
+                                        (value) => text.text=value}
+                                    width={width}
+                                    height={height}
+                                    selected={text.key === selectedId}
+                                    onTextResize={(newWidth, newHeight) => {
+                                        setWidth(newWidth);
+                                        setHeight(newHeight);
+                                    }}
+                                    onClick={ () => { selectShape(text.key) }}
+                                    onTextClick={() => { 
+                                        //eventuell muss auch hier der text übergeben werden
+                                        selectShape(text.key) }}
+                                />
+                            );
+                        })}
                     </Layer>
                 </Stage>
             </div>
             <div>
                 <form>
                     <label>Text hinzufügen</label><br/>
-                    <input type="text" value={inputtext || ""} onChange={(e) => setInputtext(e.target.value)}/><br/>
+                    <input type="text" value={input_text || ""} onChange={(e) => setInputs_Text(e.target.value)}/><br/>
                     <button onClick={(e) => {
                         e.preventDefault();
-                        setInputs( arr => [...arr, inputtext])
+                        setMytexts( arr => [...arr, {text:input_text,key:"Text_" + mytexts.length}])
                     }}>Submit</button>
                     <br/>
                     <img
@@ -99,15 +110,28 @@ const Editor = () => {
                         src="https://konvajs.org/assets/lion.png"
                         draggable="true"
                         onClick={(e) => {
-                            setImageSrc(e.target.src)
+                            e.preventDefault();
+                            setInput_Image(e.target.src)
                         }}
+                        stroke="black"
+                    />
+                    <img 
+                        alt="yoda"
+                        src="https://konvajs.org/assets/yoda.jpg"
+                        draggable="true"
+                        onClick={(e) => {
+                            setInput_Image(e.target.src)
+                        }}
+                        stroke="black"
                     />
                     <br/>
                     <button onClick={(e) => {
                         e.preventDefault();
-                        setImages( img => [...img, imagesrc])
+                        setImages( arr => [...arr, {src: input_image, key:"Image_" + images.length}])
+                        
                     }}>Submit Image</button>
-                    <p>{inputtext}</p>
+                    <p>{input_text}</p>
+                    <p>{input_image}</p>
                     
                 </form>
             </div>
