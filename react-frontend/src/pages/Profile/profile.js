8a84a8883c689c3212d5f97f7cf2c01dc82f5100
Lukas Schneider <lukas.schneider96@gmail.com>
2023-01-15 18:04:27 +0100

    profile only visible if logged in

--- react-frontend/src/pages/Profile/profile.js
+++ react-frontend/src/pages/Profile/profile.js
@@ -1,53 +1,66 @@
 import React, { useState } from 'react';
-import { Container, Grid, Typography, Paper, Box } from '@mui/material';
+import { Container, Grid, Typography, Paper, Box, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 
 const Profile = (props) => {
-    
-    
-    // ------   get User Data   ----- // 
-    return (
-        <Container>
-            <Paper elevation={1} style={{ padding: "25px", margin: "20px" }}>
-                <Typography variant="h5">Profile</Typography>
-                <Box>
-                    <Typography variant="subtitle2" style={{ display: "inline" }}>E-Mail </Typography>
-                    <p style={{ display: "inline" }}>{props.user.email}</p>
-                </Box>
-                <Box>
-                    <Typography variant="subtitle2" style={{ display: "inline" }}>Firstname </Typography>
-                    <p style={{ display: "inline" }}>{props.user.firstname}</p>
-                </Box>
-                <Box>
-                    <Typography variant="subtitle2" style={{ display: "inline" }}>Lastname </Typography>
-                    <p style={{ display: "inline" }}>{props.user.lastname}</p>
-                </Box>
-            </Paper>
 
-            <Paper elevation={1} style={{ padding: "25px", margin: "20px" }}>
-                <Typography variant="h5" style={{ marginBottom: "25px" }}>Memes History</Typography>
-                <Box sx={{ flexGrow: 1 }}>
-                    <Grid container spacing={3}>
-                        <Grid item xs>
-                            <Box style={{ height: "300px", textAlign: 'center', border: "dotted" }}>Meme 1</Box>
+
+    // ------   get User Data   ----- // 
+    if (props.isSignedIn === true) {
+        return (
+            <Container>
+                <Paper elevation={1} style={{ padding: "25px", margin: "20px" }}>
+                    <Typography variant="h5">Profile</Typography>
+                    <Box>
+                        <Typography variant="subtitle2" style={{ display: "inline" }}>E-Mail </Typography>
+                        <p style={{ display: "inline" }}>{props.user.email}</p>
+                    </Box>
+                    <Box>
+                        <Typography variant="subtitle2" style={{ display: "inline" }}>Firstname </Typography>
+                        <p style={{ display: "inline" }}>{props.user.firstname}</p>
+                    </Box>
+                    <Box>
+                        <Typography variant="subtitle2" style={{ display: "inline" }}>Lastname </Typography>
+                        <p style={{ display: "inline" }}>{props.user.lastname}</p>
+                    </Box>
+                </Paper>
+
+                <Paper elevation={1} style={{ padding: "25px", margin: "20px" }}>
+                    <Typography variant="h5" style={{ marginBottom: "25px" }}>Memes History</Typography>
+                    <Box sx={{ flexGrow: 1 }}>
+                        <Grid container spacing={3}>
+                            <Grid item xs>
+                                <Box style={{ height: "300px", textAlign: 'center', border: "dotted" }}>Meme 1</Box>
+                            </Grid>
+                            <Grid item md={4}>
+                                <Box style={{ height: "300px", textAlign: 'center', border: "dotted" }}>Meme 2</Box>
+                            </Grid>
+                            <Grid item md={4}>
+                                <Box style={{ height: "300px", textAlign: 'center', border: "dotted" }}>Meme 3</Box>
+                            </Grid>
+                            <Grid item md={4}>
+                                <Box style={{ height: "300px", textAlign: 'center', border: "dotted" }}>Meme 4</Box>
+                            </Grid>
+                            <Grid item md={4}>
+                                <Box style={{ height: "300px", textAlign: 'center', border: "dotted" }}>Meme 5</Box>
+                            </Grid>
                         </Grid>
-                        <Grid item md={4}>
-                            <Box style={{ height: "300px", textAlign: 'center', border: "dotted" }}>Meme 2</Box>
-                        </Grid>
-                        <Grid item md={4}>
-                            <Box style={{ height: "300px", textAlign: 'center', border: "dotted" }}>Meme 3</Box>
-                        </Grid>
-                        <Grid item md={4}>
-                            <Box style={{ height: "300px", textAlign: 'center', border: "dotted" }}>Meme 4</Box>
-                        </Grid>
-                        <Grid item md={4}>
-                            <Box style={{ height: "300px", textAlign: 'center', border: "dotted" }}>Meme 5</Box>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Paper>
-        </Container>
-    );
+                    </Box>
+                </Paper>
+            </Container>
+        );
+    }
+    else {
+        return (
+            <>
+                <h2>You need to login to see your profile page</h2>
+                <Link to="/signin">
+                    <Button>Login</Button>
+                </Link>
+            </>
+        )
+    }
 }
 
 export default Profile;
