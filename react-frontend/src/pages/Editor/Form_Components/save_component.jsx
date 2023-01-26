082b9230297f79ebc28cd7c262ba412adc98e9f3
Tobias Nitzl <t.nitzl@campus.lmu.de>
2023-01-26 14:36:53 +0100

    button um meme zum server zu schicken erstellt

--- react-frontend/src/pages/Editor/Form_Components/save_component.jsx
+++ react-frontend/src/pages/Editor/Form_Components/save_component.jsx
@@ -28,9 +28,39 @@ function Save_Form({ stageRef }) {
     //console.log(dataURL);
   };
 
+  const sendtoServer = () => {
+    if (!stageRef.current) {
+      console.log("Stageref ist null")
+      return;
+    }
+    const stage = stageRef.current;
+    const dataURL = stage.toDataURL({
+      mimeType: 'image/png',
+      quality: 1
+    });
+
+    createMeme(dataURL)
+  }
+
+  const createMeme = (image) => {
+    fetch('http://localhost:3002/memes', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email: "test", // TODO
+        image_encoded: image
+      })
+    })
+      .then(response => response.json())
+      .then(createdMeme => {
+        console.log(createdMeme);
+      })
+  }
+
   return (
     <div>
         <p> Hier kann das Borad gespeichert werden</p>
+        <button onClick={() => sendtoServer()}> send to server </button>
         <button onClick={() => handleSave(0.001)}>low</button>
         <button onClick={() => handleSave(0.05)}>medium</button>
         <button onClick={() => handleSave(1)}>hight</button>
