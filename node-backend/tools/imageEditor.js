f925d7ed82717f9161b79a3b190b48a1627bc39b
Gabriel Knoll <gabriel.knoll@icloud.com>
2023-02-01 16:47:05 +0100

    Basic setup for advanced

--- node-backend/tools/imageEditor.js
+++ node-backend/tools/imageEditor.js
@@ -1,17 +1,16 @@
 const Jimp = require("jimp");
 
-async function addCaptionToImage(buffer) {
-    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-    const image = Jimp.read(buffer)
-        .then(img => {
-            return img
-                .print(font, 10, 10, 'Hello World!');
-        });
-    return (await image).getBufferAsync(Jimp.MIME_JPEG)
-
-}
-module.exports = {
-    addCaptionToImage: function (buffer) {
-        return addCaptionToImage(buffer)
+module.exports = class ImageEditor {
+    constructor(buffer) {
+        this.buffer = buffer;
+    }
+    async addCaptionToImage() {
+        const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+        const image = Jimp.read(this.buffer)
+            .then(img => {
+                return img
+                    .print(font, 10, 10, 'Hello World!');
+            });
+        return (await image).getBufferAsync(Jimp.MIME_JPEG)
     }
 }
