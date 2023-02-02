47f3b0246f06631ba222e7bfde885b690b5c0f4c
Lukas Schneider <lukas.schneider96@gmail.com>
2023-02-02 21:28:37 +0100

    increased req + res limit serverside

--- node-backend/app.js
+++ node-backend/app.js
@@ -29,6 +29,8 @@ const app = express()
 const port = 3002
 
 app.listen(port, () => console.log('Example app listening on port ' + port))
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Setup  MongoDB:
 const MongoClient = require('mongodb').MongoClient;
