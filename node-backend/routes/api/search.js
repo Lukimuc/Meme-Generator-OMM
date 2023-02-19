5e97ffd38c83136c630b01cc6b6fa7c56743d8fb
Gabriel Knoll <gabriel.knoll@icloud.com>
2023-02-19 23:56:34 +0100

    Added basic exif functionality

--- node-backend/routes/api/search.js
+++ node-backend/routes/api/search.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const {findMemesInDB} = require('../../tools/dbApiSearch');
 const {zipBuffers} = require("../../tools/imageZipper");
+const {readExif} = require("../../tools/exifEditor");
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     findMemesInDB(req)
         .then((result) => {
-            const imageBuffers = result.filter((entry) => entry['image_encoded'] !== null) .map((meme) => {
+            const imageBuffers = result.filter((entry) => entry['image_encoded'] !== null).map((meme) => {
                 return convertDataUrlToImageBuffer(meme['image_encoded'])
             })
+            imageBuffers.forEach((buffer) => readExif(buffer));
             zipBuffers(imageBuffers, res)
                 .then((archive) => res.end(archive));
         });
