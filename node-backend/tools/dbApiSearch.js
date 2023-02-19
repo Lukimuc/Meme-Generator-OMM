9c90ab2e6b3f22b966a67cfe68afade7d33bc28f
Gabriel Knoll <gabriel.knoll@icloud.com>
2023-02-19 23:14:02 +0100

    Added dbApiSearch tool

---
+++ node-backend/tools/dbApiSearch.js
@@ -0,0 +1,29 @@
+//dbApiSearch.js
+function findMemesInDB(req) {
+    let searchParams = {};
+    let sortParams = {};
+    let limit = 10;
+
+    console.log(req.query);
+    if (req.query.title) {
+        searchParams['title'] = {$regex: req.query.title, $options: 'i'};
+    }
+
+    if (req.query.creator) {
+        searchParams['CreatorMail'] = req.query.creator;
+    }
+
+    if (req.query.order === "ascending") {
+        sortParams['memeCreated'] = 1;
+    } else {
+        sortParams['memeCreated'] = -1;
+    }
+
+    if (req.query.limit) {
+        limit = parseInt(req.query.limit);
+    }
+
+    return req.mongoDB.collection('memes').find(searchParams).sort(sortParams).limit(limit).toArray();
+}
+
+module.exports.findMemesInDB = findMemesInDB
